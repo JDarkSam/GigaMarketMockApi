7ac8c9700703504225bf889fdaafddd68109b8f7
Sam <jayi.darksam@gmail.com>
2025-04-27 23:49:23 -0500

    se coloca variable puerto

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const middlewares = jsonServer.defaults()
  
 server.use(middlewares)
 server.use('/api', router)
-server.listen(process.env.PORT || 3001, () => {
+server.listen(process.env.PORT || 4000, () => {
   console.log('JSON Server is running')
+  console.log('Server is running on port ${port}')
 })
